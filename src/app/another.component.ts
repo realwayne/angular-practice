a206d915e293eba41e8819b1216607ace4549581
realwayne <72178025+realwayne@users.noreply.github.com>
2022-09-04 15:37:10 +0800

    added "another.component" just testing the codesandbox flow

---
+++ src/app/another.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-another",
+  template: ` <h1>Another Component</h1> `
+})
+export class AnotherComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { AnotherComponent } from "./another.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AnotherComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
